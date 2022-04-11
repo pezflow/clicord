d417f97ff7198ca91fde68f3c7a66029ad4bb595
pezflow <82067160+pezflow@users.noreply.github.com>
2022-04-11 22:56:44 +0100

    Updated UI
    Updated UI functionality and fixed scrolling issue

--- ui.py
+++ ui.py
@@ -1,10 +1,16 @@
 from rich.layout import Layout
 from rich.panel import Panel
 from pynput import keyboard
+import json
 
-allowed_keys = list("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!" + '"' + "$%^&*(){}[]-_+='.,:;@#~|")
+
+allowed_keys = list("1234567890-=/*-!" + '"' + "£$%^&*()_+qwertyuiop[]#QWERTYUIOP{}~asdfghjkl;'ASDFGHJKL:@zxcvbnm,./ZXCVBNM<>? \|")
 class UI:
     def __init__(self):
+        
+        self.settings_file = open("settings.json")
+        self.settings = json.loads(self.settings_file.read())
+
         self.layout = Layout()
         self.layout.split_column(
             Layout(name="top"),
@@ -23,16 +29,19 @@ class UI:
         self.layout["top"].ratio = 12
         self.layout["bottom"]["input"].ratio = 4
 
-        self.server_name = "connecting..."
+        self.guild_name = "connecting..."
         self.channel_name = "connecting..."
         self.username = "user#9999"
-        self.channels = ["#general","#commands","#help"]
+        self.channels = []
         self.text_log = []
 
         self.inputted_text = ""
         self.inputkeys = []
         self.send_message = False
-        self.i = 0
+
+        self.username_color = "[" + self.settings["username_color"] + "]" 
+        self.panel_title_color = "[" + self.settings["panel_title_color"] + "]"
+        self.text_color = "[" + self.settings["text_color"] + "]"
 
         self.refresh()
     
@@ -57,14 +66,26 @@ class UI:
         self.inputted_text = "".join(self.inputkeys)
 
     def append_text_log(self,author,text):
-        self.text_log.append(f"[red]{author}:[white] {text}")
-        if len(self.text_log) > 25:
-            self.text_log.pop(0)
+        self.text_log.append(f"{self.username_color}{author}:{self.text_color} {text}")
+        if "\n".join(self.text_log).count("\n") > 25:
+            while "\n".join(self.text_log).count("\n") > 25:
+                self.text_log.pop(0)
         
     def refresh(self):
-        self.layout["top"]["left"].update(Panel("\n".join(self.channels),title=f"[white]{self.server_name}",border_style="purple"))
+        self.layout["top"]["left"].update(Panel("\n".join(self.channels),title=f"{self.panel_title_color}{self.guild_name}",border_style=self.settings["accent_color"]))
 
-        self.layout["bottom"]["input"].update(Panel(self.inputted_text,title=f"[white]Talk in #{self.channel_name}",border_style="purple"))
-        self.layout["bottom"]["account"].update(Panel(self.username,title="[white]Your account",border_style="purple"))
-        self.layout["top"]["main"].update(Panel("\n".join(self.text_log),title=f"[white]#{self.channel_name}",border_style="purple"))
+        self.layout["bottom"]["input"].update(Panel(
+            self.inputted_text,
+            title=f"{self.panel_title_color}Talk in #{self.channel_name}",
+            border_style=self.settings["accent_color"]))
+
+        self.layout["bottom"]["account"].update(Panel(
+            self.username,
+            title=f"{self.panel_title_color}Your account",
+            border_style=self.settings["accent_color"]))
+
+        self.layout["top"]["main"].update(Panel(
+            "\n".join(self.text_log),
+            title=f"{self.panel_title_color}#{self.channel_name}",
+            border_style=self.settings["accent_color"]))
     
